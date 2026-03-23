21df473eba16e929da46793f56d04aa4d164d61d
Artem Sokur <teebdol@bk.ru>
2026-03-24 01:34:06 +0400

    Забыл, что нужн делать коммиты. Ну чота сделяль короче.

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const sum = (a, b) => a + b;
+const display = document.querySelector(".display");
+const numbers = document.querySelector(".numbers");
+const opers = document.querySelector(".opers");
+const controls = document.querySelector(".controls");
 
-const minus = (a, b) => a - b;
+const sum = (a, b) => +a + +b;
 
-const umnozh = (a, b) => a * b;
+const minus = (a, b) => +a - +b;
 
-const razdelit = (a, b) => a / b;
+const umnozh = (a, b) => +a * +b;
 
-let first;
-let second;
-let oper;
+const razdelit = (a, b) => +a / +b;
 
-const operate = (first, second, oper) => {
+let first = "";
+let second = "";
+let oper = "";
+let result;
+let displayed = "";
+let currentMode = "firstMode";
+
+const calc = () => {
   switch (oper) {
     case "+":
       return sum(first, second);
@@ -22,3 +30,74 @@ const operate = (first, second, oper) => {
       return razdelit(first, second);
   }
 };
+
+const changeFirst = (num) => (first = num);
+const changeSecond = (num) => (second = num);
+const changeOper = (num) => (oper = num);
+const test = (num) => (display.textContent = num);
+const clear = () => {
+  first = "";
+  second = "";
+  oper = "";
+  result = undefined;
+  displayed = "0";
+  currentMode = "firstMode";
+  updateUI();
+};
+
+const updateUI = () => {
+  if (result) {
+    displayed = first + oper + second + "=" + result;
+  } else {
+    displayed = first + oper + second;
+  }
+  display.textContent = displayed;
+};
+
+numbers.addEventListener("click", (e) => {
+  if (e.target !== numbers) {
+    switch (currentMode) {
+      case "firstMode":
+        first += e.target.textContent;
+        updateUI();
+        break;
+      case "secondMode":
+        second += e.target.textContent;
+        updateUI();
+        break;
+    }
+  }
+});
+
+opers.addEventListener("click", (e) => {
+  if (e.target !== opers) {
+    if (second === "") {
+      oper = e.target.textContent;
+      currentMode = "secondMode";
+      updateUI();
+    } else {
+      result = calc();
+      oper = e.target.textContent;
+      first = result;
+      result = undefined;
+      second = "";
+      currentMode = "secondMode";
+      updateUI();
+      console.log(first, second, oper);
+    }
+  }
+});
+
+controls.addEventListener("click", (e) => {
+  if (e.target !== controls) {
+    switch (e.target.textContent) {
+      case "=":
+        result = calc();
+        updateUI();
+        break;
+      case "C":
+        clear();
+        updateUI();
+    }
+  }
+});
